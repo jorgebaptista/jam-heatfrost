6d3d6c20232c7e7467279222da07b1690cf7b173
Giorgi Beriashvili <giorgi.beriashvili@outlook.com>
2018-06-18 19:01:38 +0400

    Reworked the obstacle spawning system. Reorganised scene tree structure.

---
+++ HeatFrost/Scripts/Obstacle.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+export var vertical_speed = 200
+
+var motion = Vector2()
+
+# {Side} Obstacle Position
+const LF_OBSTACLE_POS = Vector2(120, -32)
+const MD_OBSTACLE_POS = Vector2(250, -32)
+const RT_OBSTACLE_POS = Vector2(380, -32)
+
+var spawn_position = [LF_OBSTACLE_POS,
+ 					  MD_OBSTACLE_POS,
+ 				      RT_OBSTACLE_POS]
+
+func _ready():
+	position = spawn_position[randi() % spawn_position.size()]
+
+func _physics_process(delta):
+	motion.y = vertical_speed
+	
+	move_and_slide(motion)
--- HeatFrost/Scripts/Spawner.gd
+++ HeatFrost/Scripts/Spawner.gd
@@ -1,12 +1,10 @@
 extends Timer
 
-var spawn_items = [
-	preload("res://Scenes/Obstacle.tscn")
-]
+var spawn_item = preload("res://Scenes/Obstacle.tscn")
 
 func _ready():
     connect("timeout", self, "_on_timeout")
 	
 func _on_timeout():
-    var item = spawn_items[0].instance()
+    var item = spawn_item.instance()	
     get_parent().add_child(item)
