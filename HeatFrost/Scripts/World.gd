a8340faa8a8b291226f2ac17fd14d62e5ef21996
Belz <jorge-baptista@outlook.com>
2018-06-19 02:57:52 +0100

    Fixed bug with score not resetting each game.

--- HeatFrost/Scripts/World.gd
+++ HeatFrost/Scripts/World.gd
@@ -1,6 +1,10 @@
 extends Node
 
+onready var score_text = get_node("/root/World/HUD/Score")
+
 func _ready():
 	global.difficulty = 0
 	global.game_is_on = true
+	global.score = 0
+	score_text.update()
 
